1d986ceb7f9da33b44c1f27318236905d14a6f3d
落雨 <69842604+MoeLuoYu@users.noreply.github.com>
2023-02-28 20:32:49 +0800

    修复了一些已知问题

--- file.php
+++ file.php
@@ -2,12 +2,12 @@
 /*
  Author:AroRain(MoeLuoYu)
  This is free software,do not use it for business.
- $ id: FileShareSystem_file 2022-4-10 CST MoeLuoYu $
+ $ id: FileShareSystem_file 2023-2-28 CST MoeLuoYu $
 */
 //import
 require "./include/config.php";
 $lang = LANG;
-require "{$lang}";
+require "./lang/{$lang}.php";
 $fileinfo = fileinfo;
 $backdir = backdir;
 $filename = filename;
@@ -20,47 +20,47 @@ $md5value = md5value;
 $sha1value = sha1value;
 //main
 if (!isset($_GET['path'])) {
-    header("Location: ./404.php");
-    exit;
+	header("Location: ./404.php");
+	exit;
 } elseif (is_dir($path = ___realpath(trim($_GET['path'])))) {
-    header("Location: ./404.php");
-    exit;
+	header("Location: ./404.php");
+	exit;
 }
 $fs = new filesystem($path);
 xhtml_head(___shortpath($path));
 if (!($data = $fs->getpath($path))) {
-    echo "<div class=\"error\">\n";
-    echo "[<a href=\"./index.php?path=" . urlencode($getcwd) . "\">{$backdir}</a>]\n";
-    echo "</div>\n";
+	echo "<div class=\"error\">\n";
+	echo "[<a href=\"javascript:;\" onClick=\"javascript :history.back(-1);\">{$backdir}</a>]\n";
+	echo "</div>\n";
 } else {
-    echo "<a href=\"./index.php?path=" . urlencode(dirname($path)) . "\">{$backdir}</a>\n";
-    echo "<div class=\"like\">\n";
-    echo "{$fileinfo}\n";
-    echo "</div>\n";
-    echo "<div class=\"rain\">\n";
-    echo "{$filename}：" . ___basename($path) . "<br />\n";
-    echo "{$sizefile}：" . ___filesize($data['size']) . "<br />\n";
-    if (function_exists("mime_content_type")) echo "{$mime}：" . mime_content_type($path) . "<br />\n";
-    echo "{$latest}：" . gmdate("Y-m-d H:i:s", ($data['atime']) + TIME) . "<br />\n";
-    echo "{$latestedit}：" . gmdate("Y-m-d H:i:s", ($data['mtime']) + TIME) . "<br />\n";
-    echo "</div>\n";
-    echo "<div class=\"like\">\n{$md5hash}\n</div>\n";
-    echo "<div class=\"rain\">\n";
-    echo "md5：";
-    if (isset($_GET['md5'])) {
-        echo "<br />" . md5_file($path);
-    } else {
-        echo "<a href=\"./file.php?{$_SERVER['QUERY_STRING']}&md5\">{$md5value}</a>\n";
-    }
-    echo "</div>\n";
-    echo "<div class=\"rain\">\n";
-    echo "sha1：";
-    if (isset($_GET['sha1'])) {
-        echo "<br />" . sha1_file($path);
-    } else {
-        echo "<a href=\"./file.php?{$_SERVER['QUERY_STRING']}&sha1\">{$sha1value}</a>\n";
-    }
-    echo "</div>\n";
+	echo "<a href=\"javascript:;\" onClick=\"javascript :history.back(-1);\">{$backdir}</a>\n";
+	echo "<div class=\"like\">\n";
+	echo "{$fileinfo}\n";
+	echo "</div>\n";
+	echo "<div class=\"rain\">\n";
+	echo "{$filename}：" . ___basename($path) . "<br />\n";
+	echo "{$sizefile}：" . ___filesize($data['size']) . "<br />\n";
+	if (function_exists("mime_content_type")) echo "{$mime}：" . mime_content_type($path) . "<br />\n";
+	echo "{$latest}：" . gmdate("Y-m-d H:i:s", ($data['atime']) + TIME) . "<br />\n";
+	echo "{$latestedit}：" . gmdate("Y-m-d H:i:s", ($data['mtime']) + TIME) . "<br />\n";
+	echo "</div>\n";
+	echo "<div class=\"like\">\n{$md5hash}\n</div>\n";
+	echo "<div class=\"rain\">\n";
+	echo "md5：";
+	if (isset($_GET['md5'])) {
+		echo "<br />" . md5_file($path);
+	} else {
+		echo "<a href=\"./file.php?{$_SERVER['QUERY_STRING']}&md5\">{$md5value}</a>\n";
+	}
+	echo "</div>\n";
+	echo "<div class=\"rain\">\n";
+	echo "sha1：";
+	if (isset($_GET['sha1'])) {
+		echo "<br />" . sha1_file($path);
+	} else {
+		echo "<a href=\"./file.php?{$_SERVER['QUERY_STRING']}&sha1\">{$sha1value}</a>\n";
+	}
+	echo "</div>\n";
 }
 xhtml_footer();
 ?>
