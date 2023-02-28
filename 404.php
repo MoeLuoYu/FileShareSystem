eebc2bcb20e2ca62c28ee95a64add2d45eabf15c
落雨 <69842604+MoeLuoYu@users.noreply.github.com>
2023-02-28 20:30:20 +0800

    优化代码结构

--- 404.php
+++ 404.php
@@ -1,17 +1,14 @@
 <?php
 /*
  Author:AroRain(MoeLuoYu)
- This is free software,but you can use it for business.
- $ id: FileShareSystem_404page 2022-4-10 CST MoeLuoYu $
+ This is free software,do not use it for business.
+ $ id: FileShareSystem_404page 2023-2-28 CST MoeLuoYu $
 */
 require "./include/config.php";
 $lang = LANG;
-require "{$lang}";
+require "./lang/{$lang}.php";
 $notfound = notfound;
-
-xhtml_head("ERROR 404 !");
-
-echo "<div class=\"error\">{$notfound}- ERROR 404 !</div>";
-
+xhtml_head("404 Not Found");
+echo "<div class=\"error\">{$notfound} - 404 Not Found</div>";
 xhtml_footer();
 ?>
